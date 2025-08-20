2cc2db9314bd7d850a37416aa301e8d556577b7d
binkim00 <k0b0301@naver.com>
2025-08-20 16:45:20 +0900

    보류...해당 커밋 풀 병합 XXXXXX 하지마세요

---
+++ rentex/src/main/java/com/rentex/penalty/exception/PenaltyExceededException.java
@@ -0,0 +1,11 @@
+//// com.rentex.penalty.exception.PenaltyExceededException
+//package com.rentex.penalty.exception;
+//
+//import com.rentex.global.exception.CustomException;
+//import com.rentex.global.exception.ErrorCode;
+//
+//public class PenaltyExceededException extends CustomException {
+//    public PenaltyExceededException() {
+//        super(ErrorCode.FORBIDDEN_PENALTY_EXCEEDED);
+//    }
+//}
